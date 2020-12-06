4f9b3a8db7173cfa193c31652e44dc0c13eb6c38
Wallace dos Anjos <wallace.anjos@al.infnet.edu.br>
2020-12-06 20:11:31 -0300

    Add project files.

---
+++ AmazingTaskList.Domain/Entities/TaskItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmazingTaskList.Domain.Entities
+{
+    public class TaskItem
+    {
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+    }
+}
---
+++ AmazingTaskList.Infrastructure/AmazingTaskListDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AmazingTaskList.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace AmazingTaskList.Infrastructure
+{
+    public class AmazingTaskListDbContext : DbContext
+    {
+        public DbSet<TaskItem> TaskItems { get; set; }
+
+        public AmazingTaskListDbContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AmazingTaskListDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=FalseData Source=(localdb)\MSSQLLocalDB;Initial Catalog=AmazingTaskListDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
